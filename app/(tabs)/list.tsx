7da40f2050fc01e6bd1905e606e952b04dd43c24
Gabriel-Dev-JS <GabrielAdsCamp@gmail.com>
2026-02-10 15:43:50 -0300

    retirando as linhas comentadas e console log

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,104 +1,3 @@
-// import { Image } from 'expo-image';
-// import { Platform, StyleSheet } from 'react-native';
-
-// import { HelloWave } from '@/components/hello-wave';
-// import ParallaxScrollView from '@/components/parallax-scroll-view';
-// import { ThemedText } from '@/components/themed-text';
-// import { ThemedView } from '@/components/themed-view';
-// import { Link } from 'expo-router';
-
-// export default function HomeScreen() {
-//   return (
-//     <ParallaxScrollView
-//       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-//       headerImage={
-//         <Image
-//           source={require('@/assets/images/partial-react-logo.png')}
-//           style={styles.reactLogo}
-//         />
-//       }>
-//       <ThemedView style={styles.titleContainer}>
-//         <ThemedText type="title">Welcome!</ThemedText>
-//         <HelloWave />
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-//         <ThemedText>
-//           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-//           Press{' '}
-//           <ThemedText type="defaultSemiBold">
-//             {Platform.select({
-//               ios: 'cmd + d',
-//               android: 'cmd + m',
-//               web: 'F12',
-//             })}
-//           </ThemedText>{' '}
-//           to open developer tools.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <Link href="/modal">
-//           <Link.Trigger>
-//             <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-//           </Link.Trigger>
-//           <Link.Preview />
-//           <Link.Menu>
-//             <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-//             <Link.MenuAction
-//               title="Share"
-//               icon="square.and.arrow.up"
-//               onPress={() => alert('Share pressed')}
-//             />
-//             <Link.Menu title="More" icon="ellipsis">
-//               <Link.MenuAction
-//                 title="Delete"
-//                 icon="trash"
-//                 destructive
-//                 onPress={() => alert('Delete pressed')}
-//               />
-//             </Link.Menu>
-//           </Link.Menu>
-//         </Link>
-
-//         <ThemedText>
-//           {`Tap the Explore tab to learn more about what's included in this starter app.`}
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-//         <ThemedText>
-//           {`When you're ready, run `}
-//           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-//           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-//         </ThemedText>
-//       </ThemedView>
-//     </ParallaxScrollView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   titleContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: 'absolute',
-//   },
-// });
-
-
-
 import Card from "@/components/Card";
 import { RefreshContext } from "@/contexts/reloadContext/refreshContext";
 import { TarefasProps, useDatabase } from "@/infra/useQuery";
@@ -117,7 +16,6 @@ export default function List() {
   const [filtroTarefa, setfiltroTarefa] = useState<string>("")
   const [tarefasFiltradas, setTarefasFiltradas] = useState<TarefasProps[]>([])
   const [tarefas, setTarefas] = useState<TarefasProps[]>([])
-  const [incremento, setIncremento] = useState<number>(0)
 
   useEffect(() => {
     async function list() {
@@ -131,7 +29,7 @@ export default function List() {
       }
     }
     list()
-  },[context?.refresh, incremento])
+  },[context?.refresh])
 
   
   const filtro = () => {
@@ -146,9 +44,8 @@ export default function List() {
   const deletar = async (id: number):Promise<SQLiteExecuteAsyncResult<number> | undefined> => {
     try{
       const response = await database.remove(id)
-      setIncremento(incremento + 1)
-      console.log("id: ", id)
-      console.log("response: ", response)
+      if (!context) throw new Error("Erro ao de refresh no contexto");
+      context?.incremento()
       return response 
     }catch(error:any){
       console.error("error: ", error)
@@ -164,8 +61,8 @@ export default function List() {
 
     try{
       const response = await database.updateTarefa(tarefas)
-      setIncremento(incremento + 1)
-      console.log("response: ", response)
+      if (!context) throw new Error("Erro ao de refresh no contexto");
+      context?.incremento()
       return response 
     }catch(error:any){
       console.error("error: ", error)
